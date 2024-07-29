97330f00979c061077273fa99ac7ee123c6dbbaa
John Kamau <johnwaweru092@gmail.com>
2024-07-29 15:15:38 +0300

    loops

---
+++ loops/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args){
+        // While loop
+        int x = 0;
+
+        while(x < 5){
+            System.out.println("The value of x is " + x);
+            x++;
+        }
+    }
+}
---
+++ loops/ForLoopDemo.java
@@ -0,0 +1,7 @@
+public class ForLoopDemo {
+    public static void main(String[] args){
+        for(int x = 0; x < 5; x++){
+            System.out.println("The value of x is " + x);
+        }
+    }
+}
